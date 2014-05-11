2e552a52b5aa22351ea7e0db58f05949ba8fccf1
xknlinux <xkn2012o@163.com>
2014-05-11 12:15:05 +0800

    haha

---
+++ rb_tree/rb_tree.h
@@ -0,0 +1,21 @@
+#ifndef RB_TREE_
+#define RB_TREE_
+
+typedef struct tree
+{
+    int iNum;
+    char iColor;
+    struct tree *ST_Parent;
+    struct tree *ST_Right;
+    struct tree *ST_Left;
+}RB_TREE;
+
+int Right_Rotate(RB_TREE **Root, RB_TREE *x);
+int Left_Rotate(RB_TREE **Root, RB_TREE *x);
+int Rb_Insert(RB_TREE **Root);
+int Rb_Insert_Data(RB_TREE **Root, RB_TREE *New);
+int Rb_Insert_Fixup(RB_TREE **Root, RB_TREE *New);
+int print(RB_TREE *bt, int n);
+
+
+#endif //RB_TREE_
