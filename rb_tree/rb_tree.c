eddc43a5347c004b9b1cdef583df752f124ca754
xknlinux <xkn2012o@163.com>
2014-05-15 07:14:15 +0800

    over

--- rb_tree/rb_tree.c
+++ rb_tree/rb_tree.c
@@ -27,10 +27,120 @@ int Rb_Delete(RB_TREE **Root)
 	}
 	else
 		Rb_Delete_Data(Root, f);
+	return 0;
 }
 
 int Rb_Delete_Data(RB_TREE **Root, RB_TREE *Z)
 {
+	RB_TREE *y;//指向即将被移除的节点或替代删除节点的节点
+	RB_TREE *x;//指向y的孩子（唯一）节点
+	char y_color; //记录即将被移除节点的颜色
+
+	y = Z;
+	y_color = y->iColor;
+	if (y->ST_Left == NULL)
+	{
+		x = y->ST_Right;
+		Rb_Transplant(Root, Z->ST_Right, Z);
+	}
+	else if (y->ST_Right == NULL)
+	{
+		x = y->ST_Left;
+		Rb_Transplant(Root, Z->ST_Left, Z);
+	}
+	else
+	{
+		y = Rb_Minimum(Z->ST_Right);
+		y_color = y->iColor;
+		x = y->ST_Right;
+		if (y->ST_Parent != Z)
+		{
+			Rb_Transplant(Root, y->ST_Right, y);
+			y->ST_Right = Z->ST_Right;
+			y->ST_Right->ST_Parent = y;
+		}
+		Rb_Transplant(Root, y, Z);
+		y->ST_Left = Z->ST_Left;
+		y->ST_Left->ST_Parent = y;
+		y->iColor = Z->iColor;
+	}
+	if (y_color == 'B')
+		Rb_Delete_Data_Fixup(Root, x);
+	return 0;
+}
+
+RB_TREE *Rb_FindBrother(RB_TREE *B)
+{
+	if (B = B->ST_Parent->ST_Left)
+		return B->ST_Parent->ST_Right;
+	else
+		return B->ST_Parent->ST_Left;
+}
+
+int Rb_Delete_Fixup(RB_TREE **Root, RB_TREE *x)
+{
+	RB_TREE *w;//指向x的兄弟节点
+	while (x != *Root && x->iColor == 'B')
+	{
+		if (x == x->ST_Parent->ST_Left)	//当x是左孩子
+		{
+			w = Rb_FindBrother(x);
+			if (w->iColor = 'R')
+			{
+				w->ST_Parent->iColor = 'R';
+				w->iColor = 'B';
+				Left_Rotate(Root, x->ST_Parent);
+				w = x->ST_Parent->ST_Right;
+			}
+			if (w->ST_Left->iColor == 'B' && w->ST_Right->iColor == 'B')
+			{
+				w->iColor = 'R';
+				x = x->ST_Parent;
+			}
+			else if (w->ST_Right->iColor = 'B')
+			{
+				w->iColor = 'R';
+				w->ST_Left->iColor = 'B';
+				Right_Rotate(Root, w);
+				w = Rb_FindBrother(x);
+				
+				w->iColor = x->ST_Parent->iColor;
+				x->ST_Parent->iColor = 'B';
+				w->ST_Right->iColor = 'B';
+				Left_Rotate(Root, x->ST_Parent);
+				x = *Root;
+			}
+		}
+		else			//当x是右孩子
+		{
+			w = Rb_FindBrother(x);
+			if (w->iColor = 'R')
+			{
+				w->ST_Parent->iColor = 'R';
+				w->iColor = 'B';
+				Right_Rotate(Root, x->ST_Parent);
+			}
+			if (w->ST_Left->iColor == 'B' && w->ST_Right->iColor = 'B')
+			{
+				w->iColor = 'R';
+				x = x->ST_Parent;
+			}
+			else if (w->ST_Left->iColor = 'B')
+			{
+				w->iColor = 'R';
+				w->ST_Right->iColor = 'B';
+				Left_Rotate(Root, w);
+				w = Rb_FindBrother(x);
+
+				w->iColor = x->ST_Parent->iColor;
+				x->ST_Parent->iColor = 'B';
+				w->ST_Left->iColor = 'B';
+				Right_Rotate(Root, x->ST_Parent);
+				x = *Root;
+			}
+		}
+	}
+	x->iColor = 'R';
 	return 0;
 }
 
@@ -128,6 +238,27 @@ int Rb_Insert_Fixup(RB_TREE **Root, RB_TREE *New)
     }
     (*Root)->iColor = 'B'; //最后一步将根结点设置为黑色
     return 0;
+}
+
+int Rb_Transplant(RB_TREE **Root, RB_TREE *v, RB_TREE *u)
+{
+	if (u->ST_Parent == NULL)
+		*Root = v;
+	else if (u->ST_Parent == u->ST_Parent->ST_Left)
+		u->ST_Parent->ST_Left = v;
+	else
+		u->ST_Parent->ST_Right = v;
+	if (v != NULL)
+	v->ST_Parent = u->ST_Parent;
+}
+
+RB_TREE *Rb_Minimum(RB_TREE *t)
+{
+	while (t != NULL)
+	{
+		t = t->ST_Left;
+	}
+	return t;
 }
 
 int Left_Rotate(RB_TREE **Root, RB_TREE *x)
--- rb_tree/rb_tree.h
+++ rb_tree/rb_tree.h
@@ -14,10 +14,13 @@ int Right_Rotate(RB_TREE **Root, RB_TREE *x);
 int Left_Rotate(RB_TREE **Root, RB_TREE *x);
 int Rb_Insert(RB_TREE **Root);
 int Rb_Insert_Data(RB_TREE **Root, RB_TREE *New);
-int Rb_Insert_Fixup(RB_TREE **Root, RB_TREE *New);
+int Rb_Insert_Fixup(RB_TREE **Root, RB_TREE *x);
+int Rb_Transplant(RB_TREE *Root, RB_TREE *v, RB_TREE *u); //v替换u
+RB_TREE *Rb_Minimum(RB_TREE *t)  //求出节点的后继元素
+RB_TREE *Rb_FindBrother(RB_TREE *B); //求出兄弟节点
 int Rb_Delete(RB_TREE **Root);
 int Rb_Delete_Data(RB_TREE **Root, RB_TREE *Z)
-//int Rb_Delete_Fixup(RB_TREE **Root, RB_TREE *New);
+int Rb_Delete_Fixup(RB_TREE **Root, RB_TREE *New);
 int print(RB_TREE *bt, int n);
 
 
